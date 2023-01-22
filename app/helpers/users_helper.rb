81b81d4c876c6b4bed69b50a4692452bf06a6b44
Eduardo Celino <ehcelino@gmail.com>
2023-01-22 17:58:55 -0300

    barra de navegação

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,11 +3,11 @@ module UsersHelper
   def user_links
     result = "".html_safe
     if current_user.username == "Visitante"
-      result << link_to("login", login_path)
-      result << link_to("Signup", signup_path)
+      result << link_to("Entrar", login_path)
+      result << link_to("Inscrição", signup_path)
     else
       result << link_to("Bem vindo, #{current_user.username}", user_path(current_user))
-      result << link_to("Logout", logout_path)
+      result << link_to("Sair", logout_path)
     end
     result
   end
