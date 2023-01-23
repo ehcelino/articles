056e8331451a0e612dd4e50ec2a8f7be90c62be9
Eduardo Celino <ehcelino@gmail.com>
2023-01-23 15:45:55 -0300

    Artigo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,24 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(category_params)
+    if @article.save
+      flash[:notice] = "Artigo criado com sucesso."
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity, content_type: "text/html"
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :user_id, :category_id, :content)
+  end
+
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,23 @@
 class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Categoria criada com sucesso."
+      redirect_to root_url
+    else
+      render :new, status: :unprocessable_entity, content_type: "text/html"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
