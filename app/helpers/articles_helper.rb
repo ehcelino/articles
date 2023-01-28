8ffc45e4dd0f81d692a88f6169214e9fe5deebac
Eduardo Celino <ehcelino@gmail.com>
2023-01-27 21:10:34 -0300

    Imagem anexada mostrada na página de índice

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,4 +4,18 @@ module ArticlesHelper
     truncate(strip_tags(article.content.to_s), length: word_count, separator: ' ')
   end
 
+  def rich_text(rich_text_object)
+    rich_text_object.to_s
+  end
+
+  def return_img(article_content)
+    content = rich_text(article_content)
+    img_tags = content.scan(/<img.*?src=["'](.*?)["'].*?>/)
+    if !img_tags[0].nil?
+      Rails.logger.debug("img #{img_tags[0][0]}")
+      image_url = img_tags[0][0]
+      image_tag(image_url, class: "resize")
+    end
+  end
+
 end
