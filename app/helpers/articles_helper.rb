5f86ff2c862a0758c323e8682392a60bc281171b
Eduardo Celino <ehcelino@gmail.com>
2023-01-28 18:13:46 -0300

    título e resumo do artigo como link

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,8 @@
 module ArticlesHelper
 
   def truncate_article(article, word_count = 100)
-    truncate(strip_tags(article.content.to_s), length: word_count, separator: ' ')
+    truncated = truncate(strip_tags(article.content.to_s), length: word_count, separator: ' ')
+    content_tag(:p, truncated, class:"article-body")
   end
 
   def rich_text(rich_text_object)
