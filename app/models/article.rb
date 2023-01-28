ccae251a20b7ed931baa98f8cd34013fde590b78
Eduardo Celino <ehcelino@gmail.com>
2023-01-28 17:12:47 -0300

    mudança na timezone e mostrando data de mod.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,9 @@ class Article < ApplicationRecord
   belongs_to :category
   has_many :comments, dependent: :destroy
   has_rich_text :content
+
+  def date_time_changed?
+    created_at.strftime("%Y-%m-%d %H:%M") != updated_at.strftime("%Y-%m-%d %H:%M")
+  end
+  
 end
